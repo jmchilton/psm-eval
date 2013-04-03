fc26b916e0ec46e5775d347e34756ba8cccd9709
John Chilton <jmchilton@gmail.com>
2013-04-03 10:46:26 -0500

    Test case for scan number.

--- test/test_peak_list.py
+++ test/test_peak_list.py
@@ -10,8 +10,8 @@ class PeakListTestCase(PsmeTestCase):
         first = True
         scan_source_manager = ScanSourceManager({"peak_lists": [path]})
         for scan in scan_source_manager.get_scans():
-            print scan
             if first:
                 assert scan.id == \
                     "controllerType=0 controllerNumber=1 scan=2"
+                assert scan.number == 2
             first = False
